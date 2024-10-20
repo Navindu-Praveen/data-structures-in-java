c6149a70c94f11d54d33a186e1bffec3b696b131
Navindu Praveen <talktopra@gmail.com>
2024-10-20 22:44:40 +0530

    create isfull and isempty methods
    to identifying stack is full or not

--- src/stackP/StackX.java
+++ src/stackP/StackX.java
@@ -5,10 +5,21 @@ class StackX{
 	private int maxSize;
 	private double[] stackArray;
 	
+	//constructor to initialize stack
 	public StackX(int s) {
 		top=-1;
 		maxSize=s;
 		stackArray=new double[maxSize];
 	}
+	
+	//check stack is full
+	public boolean isFull() {
+		return (top==top-1);
+	}
+	//check stack is empty
+	public boolean isEmpty() {
+		return (top==-1);
+	}
+	
 	
 }
