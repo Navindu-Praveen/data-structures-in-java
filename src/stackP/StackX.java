63e4ed8a01e0c52bffa898baf4d33de4b9c96abe
Navindu Praveen <talktopra@gmail.com>
2024-10-20 23:16:40 +0530

    change class name and create main class

--- src/stackP/StackX.java
+++ src/stackP/StackX.java
@@ -1,12 +1,12 @@
 package stackP;
 
-class StackX{
+class StackIm{
 	private int top;
 	private int maxSize;
 	private double[] stackArray;
 	
 	//constructor to initialize stack
-	public StackX(int s) {
+	public StackIm(int s) {
 		top=-1;
 		maxSize=s;
 		stackArray=new double[maxSize];
@@ -48,4 +48,19 @@ class StackX{
 			return stackArray[top];
 		}
 	}
+}
+
+public class StackX{
+	public static void main(String[] args) {
+		
+		StackIm stack1 = new StackIm(5);
+		
+		//push some values to stack
+		stack1.push(10.0);
+		stack1.push(20.0);
+		stack1.push(30.0);
+		
+		// Peek the top value without popping
+	    System.out.println("Peek: " +stack1.peek());
+	}	
 }
