4165d81d13ad58a3873e4df0f88afb73fee062be
Navindu Praveen <talktopra@gmail.com>
2024-10-20 22:36:31 +0530

    inpliment stack method

--- src/stackP/StackX.java
+++ src/stackP/StackX.java
@@ -1,3 +1,14 @@
 package stackP;
 
-
+class StackX{
+	private int top;
+	private int maxSize;
+	private double[] stackArray;
+	
+	public StackX(int s) {
+		top=-1;
+		maxSize=s;
+		stackArray=new double[maxSize];
+	}
+	
+}
