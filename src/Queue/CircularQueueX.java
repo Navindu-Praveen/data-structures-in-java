486d51cc5ac9fbfb117b9196a82da8f8892c8be3
Navindu Praveen <talktopra@gmail.com>
2024-10-21 22:39:57 +0530

    circuler queue methods isfull and isempty

--- src/Queue/CircularQueueX.java
+++ src/Queue/CircularQueueX.java
@@ -1,7 +1,41 @@
 package Queue;
 
 class  CircularQueueX {
+	private int front;
+	private int rear;
+	private int maxSize;
+	private int nItems;
+	private int[] queueArray;
 	
+	
+	
+	public CircularQueueX(int s) {
+		rear=-1;
+		front=0;
+		maxSize=s;
+		nItems=0;
+		queueArray=new int[maxSize];
+	}
+	
+	//isfull
+	public boolean isFull() {
+		return (nItems==maxSize); 
+	}
+	//isEmpty
+	public boolean isEmpty() {
+		return (nItems==0);
+		
+	}
+	//insert
+	public void insert(int j) {
+		if(isFull()) {
+			System.out.println("circuler queue is full ");
+		}else {
+			
+		}
+	}
+	//remove
+	//peek
 
 	
 //end
