dbe0ca2341dc1a006ca602959b31faa023afb047
Navindu Praveen <talktopra@gmail.com>
2024-10-24 02:04:09 +0530

    linked

--- src/LinkDataStr/LinkX.java
+++ src/LinkDataStr/LinkX.java
@@ -1,18 +1,68 @@
 package LinkDataStr;
-//create a linl class
-class LinkX {
-	private int iData;
-	private LinkX next;
+
+class Link{
+	public int iData; //data item
+	public Link next; //reference to next link in list
 	
-	public LinkX(int id) {
-		iData=id;
-		next=null;
+	public Link(int item) {
+		iData=item;
 	}
 	
 	public void displayLink() {
-		System.out.println(iData); //display data item
+		System.out.println(iData);
 	}
-	//end class link
+	
+}
+class LinkedList{
+	public Link first; //reference to first link on list
+	
+	public LinkedList() {
+		first = null;
+	}
+	
+	public boolean isEmpty() {
+		return (first==null);
+	}
+	
+	public void insertFirst(int item) {
+		Link newLink = new Link(item);
+		newLink.next=first;
+		first = newLink;
+	}
+	public void displayLink() {
+		Link current = first;
+		
+		while(current != null) {
+			current.displayLink();
+			current=current.next;
+		}
+	}
+	public void find(int key) {
+		Link current = first;
+		
+		while(current!=null) {
+			if(current.iData== key) {
+				return true;
+			}else {
+				current=current.next;
+			}
+		}
+		return false;
+	}
+	public void insert() {
+		
+	}
+	public void deleteFirst() {
+		
+	}
+//end 
 }
 
+public class LinkX {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
